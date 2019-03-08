0378b3ca4ce4981ec629e58665179a84e370c0c4
Brandon Nguyen <brandon.nguyen@ibm.com>
2019-03-08 10:18:49 -0500

    Removed unused carousel component

--- src/components/Travel/Travel.js
+++ src/components/Travel/Travel.js
@@ -4,7 +4,6 @@ import './Travel.css';
 
 import Map from './Map/Map.js';
 import {Helmet} from "react-helmet";
-import Carousel from 'react-bootstrap/Carousel';
 
 class Travel extends Component {
     render() {
@@ -17,7 +16,7 @@ class Travel extends Component {
             <div className="row">
                 <div className="col-md-3">
                     <p className="app-section-body-text">
-                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi aliquet dui id pellentesque commodo. Donec sed massa mi. Proin dictum arcu eu felis mattis cursus. Nulla eget eros et nulla fermentum condimentum non sed mi. Nullam ante turpis, tempor non orci vitae, cursus imperdiet dolor. Duis accumsan auctor dolor sed sollicitudin. Pellentesque ut arcu ullamcorper eros malesuada venenatis. In iaculis scelerisque felis. Aliquam tempus vehicula nibh sit amet malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget massa eu purus porttitor placerat et vitae felis. 
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi aliquet dui id pellentesque commodo. Donec sed massa mi. Proin dictum arcu eu felis mattis cursus. Nulla eget eros et nulla fermentum condimentum non sed mi. Nullam ante turpis, tempor non orci vitae, cursus imperdiet dolor. Duis accumsan auctor dolor sed sollicitudin. Pellentesque ut arcu ullamcorper eros malesuada venenatis. In iaculis scelerisque felis. Aliquam tempus vehicula nibh sit amet malesuada. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras eget massa eu purus porttitor placerat et vitae felis.
                     </p>
                 </div>
                 <div className="col-md-9">
