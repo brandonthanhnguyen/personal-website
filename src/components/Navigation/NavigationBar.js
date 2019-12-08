e655033426578186a6f18d40d850c5f1794d8bbb
Brandon Nguyen <brandon.nguyen@ibm.com>
2019-12-08 15:44:33 -0500

    Fixed LinkedIn icon link

--- src/components/Navigation/NavigationBar.js
+++ src/components/Navigation/NavigationBar.js
@@ -24,7 +24,7 @@ class NavigationBar extends Component {
         <Navbar.Collapse>
           <Nav className="mr-auto">
             <span className="nav-social-media">
-              <a href="https://www.linkedin.com/in/brandon-t-nguyen/" target="_blank" rel="noopener noreferrer"><img className="nav-icon" height="30" width="30" alt="LinkedInLogo" src={LinkedInLogo}/></a>
+              <a href="https://www.linkedin.com/in/brandonthanhnguyen/" target="_blank" rel="noopener noreferrer"><img className="nav-icon" height="30" width="30" alt="LinkedInLogo" src={LinkedInLogo}/></a>
               <a href="https://www.facebook.com/BrandonNguyenNC" target="_blank" rel="noopener noreferrer"><img className="nav-icon" height="30" width="20" alt="FacebookLogo"  src={FacebookLogo}/></a>
               <a href="https://twitter.com/imbrandonwin" target="_blank" rel="noopener noreferrer"><img className="nav-icon" height="30" width="30" alt="TwitterLogo"  src={TwitterLogo}/></a>
               <a href="https://www.instagram.com/imbrandonwin/" target="_blank" rel="noopener noreferrer"><img className="nav-icon" height="30" width="30" alt="InstagramLogo" src={InstagramLogo}/></a>
